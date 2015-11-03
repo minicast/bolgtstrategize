5235013722c7dd4b4ef3dac3dfae9fc00c4d43f6
minicast <minicast@gmx.com>
2015-11-03 15:45:52 +0100

    fix svg gecko graphics previousElementSibling.clientWidth = 0 bug

--- client/lib/startup.js
+++ client/lib/startup.js
@@ -1,3 +1,5 @@
 Meteor.startup(function(){
-  Session.set("mode", "strategicForm");
+  if(!Session.get('currentGame')) {
+    Session.set("currentGame", JSON.parse('{"formulaAscii":"(!x(~($y((P(x)&Q(y))|#p))|(*~q&~#Q(x))))","formula":{"ascii":"(!x(~($y((P(x)&Q(y))|#p))|(*~q&~#Q(x))))"},"structureAscii":"w:VLp,qRLv,u,tDOaMPP{a},Q{b};v:VLpRLuDOa,bMPP{},Q{a};u:VLqRLtDOb,cMPP{},Q{a};t:VLrRLvDOcMPP{},Q{b}","structure":{"ascii":"w:VLp,qRLv,u,tDOaMPP{a},Q{b};v:VLpRLuDOa,bMPP{},Q{a};u:VLqRLtDOb,cMPP{},Q{a};t:VLrRLvDOcMPP{},Q{b}"},"createdAt":"1446556849817"}'));
+  }
 });
